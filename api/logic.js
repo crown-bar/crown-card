021ddb9061117384ed3b139cdf7488950cd12ca5
kevinloffler <hi@kevinloeffler.ch>
2025-09-05 20:41:33 +0200

    update postgres connection host

--- api/logic.js
+++ api/logic.js
@@ -8,7 +8,7 @@ dotenv.config()
 // Database
 
 const pool = new Pool ({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
