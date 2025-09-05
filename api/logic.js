750c1ffb695162281927d2685404ed1624a9d31c
kevinloffler <hi@kevinloeffler.ch>
2025-09-05 19:26:58 +0200

    update db host to localhost

--- api/logic.js
+++ api/logic.js
@@ -8,7 +8,7 @@ dotenv.config()
 // Database
 
 const pool = new Pool ({
-    host: process.env.DB_HOST,
+    host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
